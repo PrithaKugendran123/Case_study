2bf8f692f475d0a5ce76d022aca237730691b5cb
Pritha <107340596+PrithaKugendran123@users.noreply.github.com>
2024-01-17 11:41:10 +0530

    Add files via upload

---
+++ CrimeAnalysisTests/GetIncidentsInDateRangeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Crime_Analysis_and_Reporting_System.Dao;
+using Crime_analysis_reporting_system.Dao;
+using Crime_analysis_reporting_system.entity;
+using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace CrimeManagementNew.GetIncidentsInDateRange
+{
+    [TestFixture]
+    public class CrimeAnalysisServiceTests
+    {
+        private ICrimeAnalysisService crimeAnalysisService;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            crimeAnalysisService = new CrimeAnalysisServiceImpl();
+        }
+
+        [Test]
+        public void GetIncidentsInDateRange()
+        {
+
+            DateTime startDate = DateTime.Now.AddDays(10);
+            DateTime endDate = DateTime.Now;
+
+            IEnumerable<Incident> result = crimeAnalysisService.GeTIncidentsInDateRange(startDate, endDate);
+
+            Assert.IsNotNull(result);
+            Assert.IsEmpty(result);
+        }
+    }
+}
---
+++ CrimeAnalysisTests/UpdateIncidentStatus.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Crime_analysis_reporting_system.entity;
+using Crime_Analysis_and_Reporting_System.Dao;
+using Crime_analysis_reporting_system.Dao;
+
+namespace CrimeManagementNew.Test
+{
+    [TestFixture]
+    public class CrimeAnalysisServiceTests
+    {
+        private ICrimeAnalysisService crimeAnalysisService;
+
+        [SetUp]
+        public void Setup()
+        {
+            crimeAnalysisService = new CrimeAnalysisServiceImpl();
+        }
+
+        [Test]
+        public void UpdateIncidentStatusValid()
+        {
+            // Arrange
+            int incidentId = 6;
+            string status = "Closed";
+
+            // Act
+            bool result = crimeAnalysisService.UpdateIncidentStatus(incidentId, status);
+
+            // Assert
+            Assert.IsTrue(result, "UpdateIncidentStatus should return true for a valid value.");
+        }
+
+        [Test]
+        public void UpdateIncidentStatusInvalid()
+        {
+            // Arrange
+            int incidentId = -1;
+            string status = "Closed";
+
+            // Act
+            bool result = crimeAnalysisService.UpdateIncidentStatus(incidentId, status);
+
+            // Assert
+            Assert.IsFalse(result, "UpdateIncidentStatus should return false for an invalid value.");
+        }
+
+    }
+}
